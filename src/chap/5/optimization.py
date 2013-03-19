97b6426f4d345751132fe253a67e4ac2972f56ec
Hong Duan <duanhong169@gmail.com>
2013-03-19 13:45:18 +0800

    成本函数

--- src/chap/5/optimization.py
+++ src/chap/5/optimization.py
@@ -37,3 +37,30 @@ def printschedule(r):
         ret = flights[(destination, origin)][r[2*d + 1]]
         print '%10s%10s %5s-%5s $%3s %5s-%5s $%3s' % (name, origin, out[0], out[1], out[2], ret[0], ret[1], ret[2])
         
+def schedulecost(sol):
+    totalprice = 0
+    latestarrival = 0
+    earliestdep = 24*60
+    
+    for d in range(len(sol)/2):
+        origin = people[d][1]
+        outbound = flights[(origin, destination)][int(sol[2*d])]
+        returnf = flights[(destination, origin)][int(sol[2*d+1])]
+        
+        totalprice += outbound[2]
+        totalprice += returnf[2]
+        
+        if latestarrival < getminutes(outbound[1]): latestarrival = getminutes(outbound[1])
+        if earliestdep > getminutes(returnf[0]): earliestdep = getminutes(returnf[0])
+        
+    totalwait = 0
+    for d in range(len(sol)/2):
+        origin = people[d][1]
+        outbound = flights[(origin, destination)][int(sol[2*d])]
+        returnf = flights[(destination, origin)][int(sol[2*d+1])]
+        totalwait += latestarrival - getminutes(outbound[1])
+        totalwait += getminutes(returnf[0]) - earliestdep
+    
+    if latestarrival < earliestdep: totalprice += 50
+    
+    return totalprice + totalwait
