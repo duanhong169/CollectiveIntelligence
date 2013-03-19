8fa581b5ce23bf58d01499d6c8e3795f69c60ad2
Hong Duan <duanhong169@gmail.com>
2013-03-19 13:57:29 +0800

    随机搜索

--- src/chap/5/optimization.py
+++ src/chap/5/optimization.py
@@ -64,3 +64,16 @@ def schedulecost(sol):
     if latestarrival < earliestdep: totalprice += 50
     
     return totalprice + totalwait
+
+def randomoptimize(domain, costf):
+    best = 999999999
+    bestr = None
+    for i in range(1000):
+        r = [random.randint(domain[i][0], domain[i][1]) for i in range(len(domain))]
+        
+        cost = costf(r)
+        
+        if cost < best:
+            best = cost
+            bestr = r
+    return bestr
