d390582cada5f9f8909b1367ebda0c2b3f14449a
Hong Duan <duanhong169@gmail.com>
2013-03-19 13:13:12 +0800

    描述题解

--- src/chap/5/optimization.py
+++ src/chap/5/optimization.py
@@ -11,6 +11,7 @@ import math
 people = [('Seymour', 'BOS'),
           ('Franny', 'DAL'),
           ('Zooey', 'CAK'),
+          ('Walt','MIA'),
           ('Buddy', 'ORD'),
           ('Les', 'OMA')]
 
@@ -28,3 +29,11 @@ def getminutes(t):
     x = time.strptime(t, '%H:%M')
     return x[3]*60 + x[4]
 
+def printschedule(r):
+    for d in range(len(r)/2):
+        name = people[d][0]
+        origin = people[d][1]
+        out = flights[(origin, destination)][r[2*d]]
+        ret = flights[(destination, origin)][r[2*d + 1]]
+        print '%10s%10s %5s-%5s $%3s %5s-%5s $%3s' % (name, origin, out[0], out[1], out[2], ret[0], ret[1], ret[2])
+        
