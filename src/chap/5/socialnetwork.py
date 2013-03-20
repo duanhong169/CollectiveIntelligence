08cc3e3b61ac6107ae4d5a2700090a6972773b69
Hong Duan <duanhong169@gmail.com>
2013-03-20 11:27:29 +0800

    网络可视化布局问题

---
+++ src/chap/5/socialnetwork.py
@@ -0,0 +1,22 @@
+'''
+Created on 2013-3-20
+
+@author: duanhong
+'''
+
+import math
+
+people = ['Charlie', 'Augustus', 'Veruca', 'Violet', 'Mike', 'Joe', 'Willy', 'Miranda']
+
+links = [('Augustus', 'Willy'),
+         ('Mike', 'Joe'), 
+         ('Miranda', 'Mike'), 
+         ('Violet', 'Augustus'), 
+         ('Miranda', 'Willy'), 
+         ('Charlie', 'Mike'), 
+         ('Veruca', 'Joe'), 
+         ('Miranda', 'Augustus'), 
+         ('Willy', 'Augustus'), 
+         ('Joe', 'Charlie'), 
+         ('Veruca', 'Augustus'), 
+         ('Miranda', 'Joe')]
