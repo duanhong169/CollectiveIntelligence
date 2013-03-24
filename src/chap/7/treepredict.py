d3317bd808d4b4b166a2b4ca1a2b8fba2bb1993f
Hong Duan <duanhong169@gmail.com>
2013-03-24 17:13:31 +0800

    拆分数据集

--- src/chap/7/treepredict.py
+++ src/chap/7/treepredict.py
@@ -28,3 +28,14 @@ class decisionnode:
         self.results = results
         self.tb = tb
         self.fb = fb
+        
+def divideset(rows, column, value):
+    split_function = None
+    if isinstance(value, int) or isinstance(value, float):
+        split_function = lambda row:row[column] >= value
+    else:
+        split_function = lambda row:row[column] == value
+        
+    set1 = [row for row in rows if split_function(row)]
+    set2 = [row for row in rows if not split_function(row)]
+    return (set1, set2)
