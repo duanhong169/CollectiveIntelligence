a82c62a11d1ffbb6eb9eb2eb41b59e70d3692397
Hong Duan <duanhong169@gmail.com>
2013-03-26 15:02:45 +0800

    使用方差来评价数值型结果的分类效果

--- src/chap/7/treepredict.py
+++ src/chap/7/treepredict.py
@@ -207,3 +207,11 @@ def mdclassify(observation, tree):
                 if v == tree.value: branch = tree.tb
                 else: branch = tree.fb
             return mdclassify(observation, branch)
+        
+def variance(rows):
+    if len(rows) == 0: return 0
+    data = [float(rows[len(row) - 1]) for row in rows]
+    mean = sum(data) / len(data)
+    variance = sum([(d - mean) ** 2 for d in data]) / len(data)
+    return variance
+
