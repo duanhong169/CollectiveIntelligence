6b439ff241392b3f381a06dff929f6a248474add
Hong Duan <duanhong169@gmail.com>
2013-03-26 10:17:18 +0800

    基尼不存度

--- src/chap/7/treepredict.py
+++ src/chap/7/treepredict.py
@@ -48,3 +48,14 @@ def uniquecounts(rows):
         results[r] += 1
     return results
 
+def giniimpurity(rows):
+    total = len(rows)
+    counts = uniquecounts(rows)
+    imp = 0
+    for k1 in counts:
+        p1 = float(counts[k1]) / total
+        for k2 in counts:
+            if k1 == k2: continue
+            p2 = float(counts[k2]) / total
+            imp += p1 * p2
+    return imp
