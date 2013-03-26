b19c79a1701dc83e8c0de73a875d578ac3ab3f60
Hong Duan <duanhong169@gmail.com>
2013-03-26 14:57:30 +0800

    当新输入数据不完整时采用的分类方法

--- src/chap/7/treepredict.py
+++ src/chap/7/treepredict.py
@@ -181,3 +181,29 @@ def prune(tree, mingain):
         if delta < mingain:
             tree.tb, tree.fb = None, None
             tree.results = uniquecounts(tb + fb)
+            
+def mdclassify(observation, tree):
+    if tree.results != None:
+        return tree.results
+    else:
+        v = observation[tree.col]
+        if v == None:
+            tr, fr = mdclassify(observation, tree.tb), mdclassify(observation, tree.fb)
+            tcount = sum(tr.values())
+            fcount = sum(fr.values())
+            tw = float(tcount) / (tcount + fcount)
+            fw = float(fcount) / (tcount + fcount)
+            result = {}
+            for k, v in tr.items(): result[k] = v * tw
+            for k, v in fr.items():
+                if k not in result: result[k] = 0
+                result[k] += v * fw
+            return result
+        else:
+            if isinstance(v ,int) or isinstance(v, float):
+                if v >= tree.value: branch = tree.tb
+                else: branch = tree.fb
+            else:
+                if v == tree.value: branch = tree.tb
+                else: branch = tree.fb
+            return mdclassify(observation, branch)
